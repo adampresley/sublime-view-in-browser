c8441ef57aebfd56da10f1e9e187dcdc1d7d92e7
Adam Presley <adam@adampresley.com>
2012-05-21 16:08:00 -0500

    Temp file only created if file doesn't exist. Default app opens current view otherwise

--- README.md
+++ README.md
@@ -2,10 +2,13 @@ Sublime Text 2 - View In Browser
 ================================
 
 *View In Browser* is a Sublime Text 2 plugin that will open whatever is in your
-current view/tab in your default web browser. A temporary file is created (in
-your default temp directory for your OS) with the extension of **.htm** and
-your browser will open it. By default the keystroke assigned to this plugin
-is *CTRL + ALT + V*.
+current view/tab. If the file current open is new and has not been saved a temporary 
+file is created (in your default temp directory for your OS) with the extension of 
+**.htm** and your browser will open it. However if the current open file is saved
+and has a name this plugin will open it in whatever you have set to handle
+its type.
+
+By default the keystroke assigned to this plugin is *CTRL + ALT + V*.
 
 ## Installation
 Using the Sublime Text 2 Package Control plugin (http://wbond.net/sublime_packages/package_control)
--- ViewInBrowserCommand.py
+++ ViewInBrowserCommand.py
@@ -1,5 +1,8 @@
 #
 # History:
+# 		5/21/2012:
+# 			- Temp file only created if view is unsaved
+#
 # 		5/18/2012:
 # 			- Initial code
 #
@@ -8,22 +11,28 @@ import os, tempfile, webbrowser
 
 class ViewInBrowserCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		#
-		# Create a temporary file to hold our contents
-		#
-		tempFile = tempfile.NamedTemporaryFile(suffix = ".htm", delete = False)
+		fileToOpen = self.view.file_name()
+		
+		if fileToOpen == None:
+			#
+			# Create a temporary file to hold our contents
+			#
+			tempFile = tempfile.NamedTemporaryFile(suffix = ".htm", delete = False)
 
-		#
-		# Get the contents of the current view
-		#
-		region = sublime.Region(0, self.view.size())
-		text = self.view.substr(region)
+			#
+			# Get the contents of the current view
+			#
+			region = sublime.Region(0, self.view.size())
+			text = self.view.substr(region)
 
-		tempFile.write(text)
-		tempFile.close()
+			tempFile.write(text)
+			tempFile.close()
+
+			fileToOpen = tempFile.name
+
 
 		# 
 		# And open.
 		#
-		print "Opening ", tempFile.name
-		webbrowser.open_new_tab(tempFile.name)
+		print "Opening ", fileToOpen
+		webbrowser.open_new_tab(fileToOpen)
